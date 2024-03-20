21014947362d426a42d57ffb6a114e78ef7a08d1
VrakasYi <vrakasyi@outlook.com>
2024-03-20 18:46:57 +0200

    set get working

--- script.js
+++ script.js
@@ -8,17 +8,17 @@ function hash(key) {
     hashCode = primeNumber * hashCode + key.charCodeAt(i);
   };
   
-  return hashCode;
+  return hashCode % 4;
 };
 
 function HashMap() {
-  let buckets = [];
-  let bucketsLength = 16;
+  let buckets = Array(4).fill(null);
+  let bucketsLength = 4;
 
   function set(key, value) {
     let hashCode = hash(key);
 
-    let index = hashCode % 16
+    let index = hashCode //% 4
     if (index < 0 || index >= bucketsLength) {
       throw new Error("Trying to access index out of bound");
     }; 
@@ -27,25 +27,33 @@ function HashMap() {
       buckets[index] = [key,value];
     };
 
+    // if (buckets[index]) {
+    //   let sameKey = false;
+    //   buckets.forEach(bucket => {
+    //     bucket.forEach(idx => {
+    //       // if (bucket[0][0] === key) {
+    //     if (idx[0] === key && sameKey === false) {}
+    //       buckets[index] = [[key,value]]
+    //       sameKey = true;
+    //     })
+    //   });
+    //   if (sameKey === true) {        
+    //     buckets[index].push([key,value]);
+    //   };
+    // };
+
     if (buckets[index]) {
-      let sameKey = false;
-      buckets.forEach(bucket => {
-        if (bucket[0][0] === key) {
-          buckets[index] = [[key,value]]
-          sameKey = true;
-        };
-      });
-      
-      if (sameKey) {        
-        buckets[index].push([key,value]);
-      };
-    };
+      buckets[index].push([key,value]);
+    } else {
+      buckets[index] = [[key,value]]
+    };    
   };
+  
 
   function get(key) {
     let hashCode = hash(key);
     return buckets[hashCode].find(x => x[0] === key)[1];
-  }
+  };
 
   return {
     set,
@@ -60,3 +68,5 @@ myTable.set('last name', 'tim');
 myTable.set('age', 5);
 myTable.set('dob', '1/2/1999');
 console.log(myTable.buckets)
+console.log(myTable.get("first name"));
+console.log(myTable.get("last name"));
