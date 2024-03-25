e8a9ca575a6da9367d2d84ec4b100815c6a30979
VrakasYi <vrakasyi@outlook.com>
2024-03-25 10:02:38 +0200

    HashMap.buckets now works as intended

--- script.js
+++ script.js
@@ -10,70 +10,63 @@ function hash(key) {
 };
 
 function HashMap() {
-  let buckets = Array(4).fill(null);  
+  let buckets = new Array(4);//.fill(null);  
+  let count = 0;
   
   function set(key, value) {
     let index = hash(key) % buckets.length;
-    // let hashCode = hash(key);
-    // let index = hashCode //% 4
+
     if (index < 0 || index >= buckets.length) {
       throw new Error("Trying to access index out of bound");
     }; 
 
-    if (buckets[index] === null) {
+    if (!buckets[index]) {
       buckets[index] = [[key, value]];
-      // buckets[index] = [key, value];
+    } 
+
+    const pairIndex = buckets[index].findIndex(pair => pair[0] === key);
+    if (pairIndex !== -1) {
+      // Key already exists, update value
+      buckets[index][pairIndex][1] = value;
     } else {
-      const sameKey = buckets[index].find(x => x[0] === key);
-      if (sameKey) {
-        sameKey[1] = value;
-      } else {
-        buckets[index].push([key,value]);
-      };
+      buckets[index].push([key,value]);
     };
     
-    let load = length();
-      if ((load / buckets.length) > 0.75) {
+    count++;
+    
+      if ((count / buckets.length) > 0.75) {
         reSize();
     };
   };
 
   function reSize() {
-    let copy = Array(buckets.length * 2).fill(null);
+    let copy = new Array(buckets.length * 2);
     //create new double size Array
     buckets.forEach(bucket => {
-      if(bucket !== null) {
-        // bucket.forEach(([key, value]) => {
-        //     let index = hash(key) % copy.length; 
-        //     if (copy[index] === null) {
-        //       copy[index] = []; // Initialize copy[index] if it's null
-        //     }
-        //     copy[index].push([key,value]);
-        //   });
-        // };
-        if (bucket !== null) {
-          bucket.forEach(([key, value]) => {
-            let index = hash(key) % copy.length;      
+      if (bucket) {
+        bucket.forEach(([key, value]) => {
+          let idx = hash(key) % copy.length;      
 
-            if (copy[index] === null) {
-              copy[index] = [[key, value]];  
-            } else {
-              const sameKey = copy[index].find(x => x[0] === key);
-              if (sameKey) {
-                sameKey[1] = value;
-              } else {
-                copy[index].push([key,value]);
-              };
-            };
-          });
-        };
+          if (!copy[idx]) {
+            copy[idx] = [[key, value]];  
+          } else {
+            // const pairIndex = buckets[idx].findIndex(pair => pair[0] === key);
+            // if (pairIndex !== -1) {
+            //   // Key already exists, update value
+            //   buckets[idx][pairIndex][1] = value;
+            // } else {
+            //   copy[idx].push([key,value]);
+            // };
+          copy[idx].push([key,value]);
+          };
+        });
       };
-      buckets = copy;
+      HashMap.buckets = copy;
     });
   };
   
   function get(key) {
-    //let index = hash(key);
+    let index = hash(key) % buckets.length;
     return buckets[index].find(x => x[0] === key)[1];
   };
 
@@ -165,16 +158,17 @@ myTable.set('first name', 'bob');
 myTable.set('last name', 'tim');
 myTable.set('age', 5);
 myTable.set('dob', '1/2/1999');
-
+console.log(myTable.get('dob'));
 console.log(myTable.buckets);
 myTable.set('a', '1');
 myTable.set('b', '2');
 myTable.set('c', '3');
 myTable.set('d', '4');
 myTable.set('f', '5');
-
+console.log(myTable.get('d'));
 console.log(myTable.buckets);
 console.log(myTable.entries());
+
 console.log(myTable.length());
 // myTable.remove('first name');
 // console.log(myTable.buckets);
